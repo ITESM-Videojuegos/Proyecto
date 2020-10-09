7603cb1147d0b1443c18b7c98b9a41b4ceef1d0c
JoelSantillan <joel7164@gmail.com>
2020-10-08 20:10:17 -0500

    added enemy AI

--- plattformer/Assets/Scripts/EnemyFire.cs
+++ plattformer/Assets/Scripts/EnemyFire.cs
@@ -1,25 +1,60 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class EnemyFire : MonoBehaviour
 {
     public Transform enemyFirePoint;
     public GameObject bullet;
-    public Boolean shoot;
+    
+    public Transform player;
+
+    Boolean bandera;
+
+    [SerializeField]
+    float agroRange;
+
+    float disToPlayer;
+
+    private IEnumerator enumerador;
+    private Coroutine corrutina;
 
     // Update is called once per frame
+    private void Start()
+    {
+        enumerador = Shoot();
+        bandera = true;
+    }
     void Update()
     {
-
-        if (this.shoot) { Shoot(); }
+        
+        disToPlayer = Vector2.Distance(transform.position, player.position);
+        
+        if (disToPlayer < agroRange && bandera)
+        {
+            corrutina = StartCoroutine(enumerador);
+            bandera = false;
+            
+        }
+        else if (disToPlayer > agroRange && !bandera)
+        {
+            StopAllCoroutines();
+            bandera = true;
+        }
 
     }
 
-    public void Shoot()
+
+    private IEnumerator Shoot()
     {
         //Logica de disparo
-        Instantiate(bullet, enemyFirePoint.position, enemyFirePoint.rotation);
+        while (true)
+        {
+            Instantiate(bullet, enemyFirePoint.position, enemyFirePoint.rotation);
+            yield return new WaitForSeconds(2.5f);
+        }
+
     }
 }
---
+++ plattformer/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Node[] path;
+    private int current;
+
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(path[current].transform);
+        transform.Translate(transform.forward * Time.deltaTime, Space.Self);
+        
+    }
+}
